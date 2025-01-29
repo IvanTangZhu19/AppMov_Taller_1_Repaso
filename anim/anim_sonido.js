ba8cc507d269faf00b4bd328f4efb6938e571075
DavidZO1 <97901534+DavidZO1@users.noreply.github.com>
2025-01-29 15:07:37 -0500

    reto animacion y sonido

--- anim/anim_sonido.js
+++ anim/anim_sonido.js
@@ -1,19 +1,34 @@
-gsap.registerPlugin(Draggable, InertiaPlugin);
+document.addEventListener('DOMContentLoaded', () => {
+  // Sonidos
+  const sounds = {
+      nube: new Audio('./bang-smoke-cloud-1-184061.mp3'),
+      bici: new Audio('./bike-bell-40094.mp3'),
+      arbol: new Audio('./hit-tree-01-266310.mp3'),
+      sol: new Audio('./surface-of-the-sun-30475.mp3')
+  };
 
-//Permite arrastrar la bicicleta en x
-Draggable.create("#bici", {
-  type: "x",
-  bounds: "#paisaje",
-});
+  // Configurar draggables
+  Draggable.create("#bici", {
+      type: "x,y",
+      bounds: "#paisaje",
+      onPress() { sounds.bici.cloneNode(true).play(); }
+  });
 
-//Permite rotar el sol
-Draggable.create("#sol", {
-  type: "rotation",
-  inertia: true
-});
+  Draggable.create("#sol", {
+      type: "rotation",
+      inertia: true,
+      onPress() { sounds.sol.cloneNode(true).play(); }
+  });
 
-//Permite arrastrar la nube
-Draggable.create("#nube", {
-  type: "x",
-  bounds: "#paisaje",
+  Draggable.create("#nube", {
+      type: "x,y",
+      bounds: "#paisaje",
+      onPress() { sounds.nube.cloneNode(true).play(); }
+  });
+
+  Draggable.create("#arbol", {
+      type: "x,y",
+      bounds: "#paisaje",
+      onPress() { sounds.arbol.cloneNode(true).play(); }
+  });
 });
